ad59e722fb4f119f30d692830202328cdc76dfdd
SeHyung Ryu <rsh9424@naver.com>
2021-11-01 12:59:53 +0900

    삭제된 작성자는 none으로 표시하기

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):#파이썬에서 제공하는 모델 사용
     file_upload= models.FileField(upload_to='blog/files/%Y/%m/%d/',blank=True)
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank=True)
     created_at = models.DateTimeField(auto_now_add=True) # 데이터베이스에 이런것들을 저장할수있도록 가능
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.ForeignKey(User,null=True,on_delete=models.SET_NULL)
     updated_at=models.DateTimeField(auto_now=True)#수정됬을때는 수정된시간만 저장, 추가됬을때는 저절로 입력
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author} '
