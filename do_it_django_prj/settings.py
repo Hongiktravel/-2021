a2fa52b73b19beb33d98382d9272124a68b26c1c
SeHyung Ryu <rsh9424@naver.com>
2021-11-05 14:57:27 +0900

    Nginx 적용하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -28,7 +28,7 @@ DEBUG = int(os.environ.get('DEBUG', 1))
 
 if os.environ.get('DJANGO_ALLOWED_HOSTS'):
     ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
